26c1b822872486038f5ba079467eca0acfd47a69
jparra55 <jparra55@unisalle.edu.co>
2016-04-03 20:28:01 -0500

    ProgAssig2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script has the two functions required in order to complete the programming assignment 2, above the functions is a brieg description of what the functions do.
 
-## Write a short comment describing this function
+## This function sets and gets a matrix, also calculates the inverse of the given matrix 2 X 2, and gets the result.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     i <- NULL
+     set <- function(y) {
+          x <<- matrix(y, 2, 2)
+          i <<- NULL
+     }
+     get <- function() x
+     setinv <- function(solve) i <<- solve
+     getinv <- function() i
+     
+     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates (if it hasn't been calculated) the inverse of the matrix created with the function above and print the result in the console, if the inverse has already been calculated, the function prints the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     i <- x$getinv()
+     if (!is.null(i)) {
+          print("The inverse has already been calculated")
+          return(i)
+     }
+     m22 <- x$get()
+     i <- solve(m22)
+     x$setinv(i)
+     i
 }
